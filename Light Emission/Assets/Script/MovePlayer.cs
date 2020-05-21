52901eb7cd273e3f5391daef05855e36f730b96b
zees98 <zeeshanhamdani98@gmail.com>
2020-05-22 02:42:28 +0500

    Initial Files
    Initial Files

---
+++ Light Emission/Assets/Script/MovePlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    private float acceleration;
+    private float turn;
+    public float speed =  20.0f;
+    public float turnMultiplier = 2.0f;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        acceleration = Input.GetAxis("Vertical");
+
+        turn = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * acceleration);
+        transform.Rotate(Vector3.up * turnMultiplier * Time.deltaTime * turn);
+
+    }
+}
