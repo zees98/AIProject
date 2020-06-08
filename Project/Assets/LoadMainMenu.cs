48b4a4ce1dbbcb65bbc7cb95af857e1982cb76bb
zees98 <zeeshanhamdani98@gmail.com>
2020-06-08 21:24:31 +0500

    9.24pm 8 Jun 2020

---
+++ Project/Assets/LoadMainMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class LoadMainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void loadMM()
+    {
+        SceneManager.LoadScene("Menu");
+
+    }
+}
