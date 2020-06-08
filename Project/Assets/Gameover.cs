195a991e32c6d57ae63ad2155d6499086ecd1181
Fahad-khalid97 <55343805+Fahad-khalid97@users.noreply.github.com>
2020-06-08 21:21:39 +0500

    changes

---
+++ Project/Assets/Gameover.cs
@@ -0,0 +1,22 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+
+// public class Gameover : MonoBehaviour
+// {
+//      public static string gameover=null;
+//     Text messagetext;
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+       
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//          messagetext.text=gameover;
+        
+//     }
+// }
---
+++ Project/Assets/Script/Fahad/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private float currenttime = 59f;
+    public float startingTime =59f;
+
+     Text timetext;
+
+    void Start()
+    {
+        timetext = GetComponent<Text> ();
+        currenttime = startingTime;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        //  Gameover.gameover="";
+        currenttime -= 1*Time.deltaTime;
+        timetext.text = currenttime.ToString("0");
+        if(currenttime<0){
+            Debug.Log("FFF");
+             timetext.text = 0f.ToString("0");
+        }
+       
+
+    }
+}
