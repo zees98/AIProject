ccaf4c9dfde9d771169ca28bfe550c01bd8dbe18
zees98 <zeeshanhamdani98@gmail.com>
2020-06-08 15:49:12 +0500

    Audio files

---
+++ Project/Assets/CarSound.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSound : MonoBehaviour
+{
+  
+    public AudioSource[] clips;
+    private bool isPlayerMoving;
+    // Start is called before the first frame update
+    void Start()
+    { 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isPlayerMoving = GameObject.Find("Player").GetComponent<MoveThePlayer>().isMoving;
+        
+        if (isPlayerMoving)
+        {
+            clips[0].Stop();
+            if (!clips[1].isPlaying)
+            {
+               
+                clips[1].Play();
+            }
+        }
+        else
+        {
+            clips[1].Stop();
+            if (!clips[0].isPlaying)
+            {
+
+                
+                clips[0].Play();
+            }
+        }
+
+       
+    }
+}
--- Project/Assets/Script/Zeeshan/MoveThePlayer.cs
+++ Project/Assets/Script/Zeeshan/MoveThePlayer.cs
@@ -9,27 +9,42 @@ public class MoveThePlayer : MonoBehaviour {
     public float gravityModifier;
     public float speed = 20.0f;
     public float turnMultiplier = 2.0f;
+    public bool turbo = false;
+    public Light turboLight;
+    public bool isMoving;
     // Start is called before the first frame update
 
     void Start () {
         Physics.gravity *= gravityModifier;
+        turboLight = GameObject.Find("Turbo Light").GetComponent<Light>();
+        
+        
     }
 
     // Update is called once per frame
     void Update () {
         acceleration = Input.GetAxis ("Vertical");
 
+        isMoving = acceleration != 0;
+
         turn = Input.GetAxis ("Horizontal");
 
-        transform.Translate (Vector3.left * speed * Time.deltaTime * acceleration);
+        transform.Translate (Vector3.left * speed * Time.deltaTime * acceleration * (turbo ? 2f: 1f));
         transform.Rotate (Vector3.up * turnMultiplier * Time.deltaTime * turn);
 
         if (Input.GetKeyDown (KeyCode.R)) {
 
             transform.rotation = Quaternion.Euler (0f, 0f, 0f);
-            transform.position = new Vector3 (transform.position.x, 1.6f, transform.position.z);
+            transform.position = new Vector3 (transform.position.x, 2.5f, transform.position.z);
 
         }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            turbo = !turbo;
+            
+        }
+        
+        turboLight.intensity = turbo ? 50 : 0;
 
     }
 }
